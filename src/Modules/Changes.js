5ba416d4e7601af7a5f6e1a3c3b3c3097d4ba79b
Allan Leal <allanjohnson.leal@gmail.com>
2021-09-12 19:20:12 -0300

    feature: add new module to change main files

---
+++ src/Modules/Changes.js
@@ -0,0 +1,10 @@
+const Changes = 
+`Titulo;Tecnologias;Experiência;
+QA-Plataform;Javascript;3;
+JS-Blog;Javascript;4;
+NLW-Together;Typescript;4;
+NLW-5;Typescript;4;
+NLW-4;Typescript;3;` 
+
+
+module.exports = Changes;
