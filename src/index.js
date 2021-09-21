5a16d169c11d42ed3bf26977b02c6d82439ddb70
cs-allan-johnson <allan.johnson.d.leal@accenture.com>
2021-09-21 13:34:10 -0300

    featyre: add Processor from transform string to arra

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const Reader = require('./Services/Reader');
+const Processor = require('./Services/Processor');
 
 const read = new Reader()
 async function main(){
     try{
         const data = await read.ReadFile('docs/myDoc.csv')
-        console.info(data)
+        const preProcessorData = Processor.Process(data);
+        console.info(preProcessorData)
     }
     catch(err){
         console.info(err, ' >>>Error in index')
